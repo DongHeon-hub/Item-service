8c05bb38d0a98898f3c8ebfb73ade0bb39e17a7f
Mengou <gjsehdrh@gmail.com>
2023-08-25 17:30:49 +0900

    add checkbox&radio&select button

--- item-service/src/main/java/hello/itemservice/domain/item/Item.java
+++ item-service/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,19 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Item {
     private Long Id;
     private String itemName;
     private Integer price;
     private Integer quantity;
+
+    private Boolean open; // 판매 여부
+    private List<String> regions; // 판매 지역
+    private ItemType itemType; // 상품 종류
+    private String deliveryCode; // 배송 방식
 
     public Item(){
     }
